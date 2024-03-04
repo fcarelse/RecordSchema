6371f4b081441e93573134395016f5476a676a58
frank <frank@js.ie>
2024-03-04 09:59:03 +0000

    Uncommitted build folders

--- lib/RecordSchema.d.ts
+++
@@ -1,9 +0,0 @@
-import { RecordSchemaType } from "./RecordSchema.types";
-export declare class RecordSchema {
-    private meta;
-    private model;
-    toJSON(): string;
-    static genFromJSON(temp: RecordSchemaType): boolean;
-    static isValidMeta(sample: any): boolean;
-    static isValidModel(sample: any): boolean;
-}
